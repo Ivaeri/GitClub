b758be97410d8f50315d42b6c2d308935c2fcb51
Your Name <you@example.com>
2024-12-03 15:14:10 +0100

    CSS och HTML på startsidan

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -8,28 +8,44 @@
       {{ uiLabels.titlegame}}
       <img src="../assets/logo.svg">
     </div>
+    <div class="languagecontainer">
+      <button v-bind:class="lang === 'sv' ? 'englishbutton' : 'swedishbutton'" v-on:click="switchLanguage"> </button>
+      <div v-on:click="switchLanguage">
+        {{ uiLabels.languagebox }}
+      </div>
+    </div>
   </header>
-  <ResponsiveNav v-bind:hideNav="hideNav">
-    <button v-on:click="switchLanguage">
-      {{ uiLabels.changeLanguage }}
-    </button>
-    <router-link to="/create/">
-      {{ uiLabels.createPoll }}
+    <div class="create-join">
+      <router-link to="/create/">
+      <button>{{ uiLabels.createGame }}</button>
+      </router-link>
+      <router-link to="/lobby/">
+      <button>{{ uiLabels.participateGame }}</button>
+      </router-link>
+    </div>
+
+
+    <ResponsiveNav v-bind:hideNav="hideNav">
+      <button v-on:click="switchLanguage">
+        {{ uiLabels.changeLanguage }}
+      </button>
+      <router-link to="/create/">
+       {{ uiLabels.createPoll }}
+      </router-link>
+      <a href="">
+       {{ uiLabels.about }}
+     </a>
+      <a href="">FAQ</a>
+    </ResponsiveNav>
+    <label>
+      Write poll id: 
+      <input type="text" v-model="newPollId">
+    </label>
+    <router-link v-bind:to="'/lobby/' + newPollId">
+      {{ uiLabels.participatePoll }}
     </router-link>
-    <a href="">
-      {{ uiLabels.about }}
-    </a>
-    <a href="">FAQ</a>
-  </ResponsiveNav>
-  <h1>{{ uiLabels["sales-pitch"] }}</h1>
-  <h2>{{ uiLabels.subHeading }}</h2>
-  <label>
-    Write poll id: 
-    <input type="text" v-model="newPollId">
-  </label>
-  <router-link v-bind:to="'/lobby/' + newPollId">
-    {{ uiLabels.participatePoll }}
-  </router-link>
+
+
 </template>
 
 <script>
@@ -73,17 +89,20 @@ export default {
 </script>
 <style scoped>
   header {
-    background-color: gray;
+    position: relative;
+    background-color: white;
     width: 100%;
+    height: 350px;
     display: grid;
     grid-template-columns: 2em auto;
+
   }
   .logo {
     text-transform: uppercase;
     letter-spacing: 0.25em;
-    font-size: 2.5rem;
-    color: white;
-    padding-top:0.2em;
+    font-size: 5rem;
+    color: black;
+    padding-top: 2.5em;
   }
   .logo img {
     height:2.5rem;
@@ -95,13 +114,80 @@ export default {
     width:1em;
     display: flex;
     align-items: center;
-    justify-content: left;
+    justify-content: center;
     padding:0.5rem;
     top:0;
     left:0;
     height: 2rem;
     cursor: pointer;
     font-size: 1.5rem;
+  }
+  .create-join {
+  background-color: transparent; 
+  display: flex;
+  color: white; 
+  border: none;
+  gap: 3em; 
+  border-radius: 10px; 
+  transition: background-color 0.3s ease; 
+  margin: auto; 
+  text-align: left; 
+  width: fit-content; 
+  margin-bottom: 10em;
+  }
+
+  .create-join button {
+  background-color: #d63384; 
+  color: white;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer; 
+  gap: 7em;
+  font-size: 1em;
+  transition: background-color 0.3s ease;
+  width: 20em;
+  height: 10em;
+  }
+.create-join button:hover {
+background-color: #a02666; 
+}
+  .languagecontainer{
+    position:absolute;
+    width: 240px;
+    height: 60px;
+    margin-top: 20px;
+    margin-right: 10px;
+    right: 0;
+  }
+  .languagecontainer div{
+    position: absolute;
+    width: 80px;
+    height: 30px;
+    right: 0;
+    margin-top: 15px;
+    margin-right: 5px;
+    background-color: lightgrey;
+    border-radius: 10%;
+    padding-top: 7px;
+    cursor: pointer;
+    border: 2px solid black
+
+  }
+  .languagecontainer button{
+    position: absolute;
+    height: 60px;
+    width: 140px;
+    background-size: cover;
+    background-position: center;
+    left: 0;
+    cursor: pointer;
+    border-radius: 10%;
+  }
+  .swedishbutton{
+    background-image: url("public/img/svenskflagga.jpg");
+  }
+  .englishbutton{
+    background-image: url("public/img/uk.png");
   }
 
 @media screen and (max-width:50em) {
@@ -113,9 +199,11 @@ export default {
   }
   .hamburger::before {
     content: "☰";
+    color:black;
   }
   .close::before {
     content: "✕";
+    color:black;
   }
   .hide {
     left:-12em;
