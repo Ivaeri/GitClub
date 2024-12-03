f457f52c2dbdf5aa5050e416b1e830c6a0600797
Hugo Josefsson <hujo8134@student.uu.se>
2024-12-03 16:55:13 +0100

    lade till enterWordHere i uiLabels och fixade lite på wordSubmission, nu finns en checkbox med länk och en textruta plus rubrik

--- src/views/WordSubmission.vue
+++ src/views/WordSubmission.vue
@@ -1,11 +1,21 @@
 <template>
     <h1>Co-op mode</h1>
-    <div>
-        <router-link to="/hostLobby/">
-          {{ uiLabels.enterWordHere }}
-        </router-link>
-    </div>
-    
+    <div class="grid">
+
+        
+       
+          
+            <input type="text" v-bind:placeholder="uiLabels.enterWordHere" v-model="word" class="textField">
+       
+       
+        
+            <router-link to="/hostLobby/">
+                <div class="link">
+                    <img src= "/img/check-mark-icon-green-0.png">  
+                </div>
+            </router-link>
+         
+        </div>
     </template>
     
     <script>
@@ -31,5 +41,33 @@
     </script>
     
     <style scoped>
-    
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(2,600px);
+        grid-gap: 20px;
+    }
+    .grid > img{
+
+    }
+    .textField {
+        background-color: lightblue;
+        width: 20em;
+        height: 10em;
+    }
+    .link {
+        background-color: lightblue;
+        width: 6em;
+        height: 6em;
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .link > img {
+        width: 80%;
+        height: 80%;
+        position: absolute;
+    }
+   
+   
     </style>
