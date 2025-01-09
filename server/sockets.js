07319c17c2448baa47f08e808b46b248b228af92
Hugo Josefsson <hujo8134@student.uu.se>
2025-01-09 15:20:12 +0100

    för pull

--- server/sockets.js
+++ server/sockets.js
@@ -33,7 +33,6 @@ function sockets(io, socket, data) {
  
   socket.on("generateId", function(d) {
     socket.emit('setPollId', data.setPollId(d))
-    //io.emit("generateId",  { pollId: d });
     io.emit("activePollsUpdate", Object.keys(data.polls));
   });
 
@@ -43,12 +42,7 @@ function sockets(io, socket, data) {
     socket.emit('pollData', data.getPoll(d.pollId));
     io.emit('activePollsUpdate', Object.keys(data.polls));
   });
-/*
-  socket.on('addQuestion', function(d) {
-    data.addQuestion(d.pollId, {q: d.q, a: d.a});
-    socket.emit('questionUpdate', data.activateQuestion(d.pollId));
-  });
-*/
+
   socket.on("removePollIdFromList", function(pollId) {
     console.log("removePollIdFromList körs i sockets.js", pollId)
     data.addToInActivePolls(pollId)
@@ -82,8 +76,7 @@ function sockets(io, socket, data) {
 
   socket.on('participateInPoll', function(d) {
     data.participateInPoll(d.pollId, d.name);
-   // io.to(d.pollId).emit('participantsUpdate', data.getParticipants(d.pollId));
-   // io.emit('participantsUpdate', data.getParticipants(d.pollId));
+   
     
   });
   socket.on('leavePoll', function(d) {
@@ -128,12 +121,7 @@ function sockets(io, socket, data) {
   socket.on('startPoll', function(pollId) {
     io.to(pollId).emit('startPoll');
   })
-  /*
-  socket.on('runQuestion', function(d) {
-    let question = data.activateQuestion(d.pollId, d.questionNumber);
-    io.to(d.pollId).emit('questionUpdate', question);
-    io.to(d.pollId).emit('submittedAnswersUpdate', data.getSubmittedAnswers(d.pollId));
-  });*/
+
 
   socket.on('NailInCoffin' , function(d) {
     data.nailInCoffin(d.userName, d.pollId);
@@ -144,12 +132,7 @@ function sockets(io, socket, data) {
     let nail = data.getNailInCoffin(pollId);
     io.emit("nail", nail);
   });
-/*
-  socket.on('submitAnswer', function(d) {
-    data.submitAnswer(d.pollId, d.answer);
-    io.to(d.pollId).emit('submittedAnswersUpdate', data.getSubmittedAnswers(d.pollId));
-  });
-*/
+
   socket.on("updateGuessedLetters", function(d){
     data.updateGuessedLetters(d.pollId, d.key);
   })
